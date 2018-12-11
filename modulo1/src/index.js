e91f20c4a7649cde33cf6b5af0cfd40018458d0e
ChilenoSika <jaum97@yahoo.com.br>
2018-12-11 01:16:51 -0200

    PropTypes

--- modulo1/src/index.js
+++ modulo1/src/index.js
@@ -1,18 +1,42 @@
 import React, { Component, Fragment } from 'react'
 import { render } from 'react-dom'
+import PropTypes from 'prop-types'
 
 class Button extends Component {
   render() {
-    return <a href="#">Save</a>
+    return (
+      <a href="#" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    )
   }
 }
 
+Button.defaultProps = {
+  children: 'Save'
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.string
+}
+
 class App extends Component {
+  handleClick() {
+    window.alert('Button clicked')
+  }
+
   render() {
     return (
       <Fragment>
         <h1> test </h1>
-        <Button />
+        <Button
+          onClick={() => {
+            window.alert('Button 1')
+          }}
+        />
+        <br />
+        <Button onClick={this.handleClick}>Send</Button>
       </Fragment>
     )
   }
