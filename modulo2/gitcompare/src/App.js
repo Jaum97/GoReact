0330c39f029b217c27aacb35d4774ff8a1790b73
ChilenoSika <jaum97@yahoo.com.br>
2018-12-22 23:31:50 -0200

    FUgged

--- modulo2/gitcompare/src/App.js
+++ modulo2/gitcompare/src/App.js
@@ -1,18 +1,18 @@
 import React, { Fragment } from 'react';
-import styled from 'styled-components';
+import Main from './pages/Main';
 import GlobalStyle from './styles/global';
 
-const Title = styled.h1`
+/* const Title = styled.h1`
   color: #f00;
   font-size: 32px;
-`;
+`; */
 
 const App = () => (
   <Fragment>
-    <GlobalStyle />
-    <div className="App">
-      <Title>Hello World</Title>
-    </div>
+    <GlobalStyle>
+      <Main />
+;
+    </GlobalStyle>
   </Fragment>
 );
 
