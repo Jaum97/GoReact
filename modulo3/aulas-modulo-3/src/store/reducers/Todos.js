e976e8aebf6cbc29b080fa48186407a7cbc7afb1
ChilenoSika <jaum97@yahoo.com.br>
2019-01-04 00:20:28 -0200

    Adding Todos

--- modulo3/aulas-modulo-3/src/store/reducers/Todos.js
+++ modulo3/aulas-modulo-3/src/store/reducers/Todos.js
@@ -3,8 +3,10 @@ const INITIAL_STATE = [
   { id: 2, text: "Estudar React" }
 ];
 
-export default function Todos(state = INITIAL_STATE, action) {
+export default function todos(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case "ADD_TODO":
+      return [...state, { id: Math.random(), text: action.payload.text }];
     default:
       return state;
   }
