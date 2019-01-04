f7b80490d5e45dcb3df33888b9674c040e4a154f
ChilenoSika <jaum97@yahoo.com.br>
2019-01-04 00:32:36 -0200

    removeTodos

--- modulo3/aulas-modulo-3/src/TodoList.js
+++ modulo3/aulas-modulo-3/src/TodoList.js
@@ -4,11 +4,14 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as TodoActions from "./store/actions/todos";
 
-const TodoList = ({ todos, addTodo }) => (
+const TodoList = ({ todos, addTodo, removeTodo }) => (
   <Fragment>
     <ul>
       {todos.map(todo => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => removeTodo(todo.id)}>remove</button>
+        </li>
       ))}
     </ul>
     <button onClick={() => addTodo("novo todo")}>add todo</button>
@@ -17,6 +20,7 @@ const TodoList = ({ todos, addTodo }) => (
 
 TodoList.PropTypes = {
   addTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
